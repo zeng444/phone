3ddd0070ffb4a881ad5bb4cfcef27c341e0c0c13
robert <zeng444@163.com>
2021-12-21 18:31:16 +0800

    init

---
+++ demo/Test.php
@@ -0,0 +1,4 @@
+<?php
+include_once dirname(__DIR__) . '/vendor/autoload.php';
+$EPhone = new \Janfish\EPhone\Client();
+//$EPhone->dial()
---
+++ src/Client.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace Janfish\EPhone;
+
+use RuntimeException;
+use Swlib\Saber;
+
+/**
+ * Class Client
+ * @author Robert
+ * @package Janfish\EPhone
+ */
+class Client
+{
+
+    private $host = 'http://new.02110000.com';
+    private $appId = '9449';
+    private $appSecret = 'SmX8SnsRsdZ6GN3SJLLImHzLZz9T5wY4';
+
+    private $proxyHost = '47.112.123.35';
+    private $proxyPort = '34491';
+
+    const DIAL_ERROR_CODE = [
+        '0' => '请求成功',
+        '99' => 'call id格式错误',
+        '101' => '系统繁忙',
+        '102' => '账号异常',
+        '103' => '账号不存在或者已禁用',
+        '104' => '余额不足',
+        '105' => '线路异常',
+        '106' => '参数错误',
+        '110' => '主叫或者被叫号码在黑名单',
+        '111' => '主叫受限（主叫被呼叫太多次)',
+        '112' => '被叫受限（被叫被呼叫太多次)',
+        '113' => '主叫或被叫号码异常',
+        '114' => '呼叫并发达到上限',
+        '116' => '呼叫区域限制或盲区',
+        '117' => 'Callid不唯一',
+        '118' => '代理商或用户不存在',
+        '119' => '代理商余额不足',
+        '120' => '请求体太大',
+        '124' => '主叫号码不允许,请联系管理员',
+        '125' => '绑定号码错误',
+        '141' => '客户端ip错误',
+        '501' => 'ip地址未认证或者token无效',
+        '999' => '其他错误',
+    ];
+
+    /**
+     * @param array $options
+     */
+    public function __construct(array $options = [])
+    {
+        if (isset($options['host'])) {
+            $this->host = $options['host'];
+        }
+        if (isset($options['appId'])) {
+            $this->appId = $options['appId'];
+        }
+        if (isset($options['appSecret'])) {
+            $this->appSecret = $options['appSecret'];
+        }
+        if (isset($options['proxyHost'])) {
+            $this->proxyHost = $options['proxyHost'];
+        }
+        if (isset($options['proxyPort'])) {
+            $this->proxyPort = $options['proxyPort'];
+        }
+    }
+
+    /**
+     * 获取token
+     * @return string
+     * @author Robert
+     */
+    public function getToken(): string
+    {
+        $body = $this->http('post', '/api/login', $this->protocol([
+            'Username' => $this->appId,
+            'Password' => $this->appSecret,
+        ]));
+        if (!$body || !$body = json_decode($body, true)) {
+            throw new RuntimeException('请求失败');
+        }
+        if (isset($body['Error'])) {
+            throw new RuntimeException($body['Error'] . ' code:' . $body['result']);
+        }
+        return $body['token'] ?? '';
+    }
+
+    /**
+     * @param string $mobile
+     * @param string $caller
+     * @param string $callee
+     * @param string $notifyUrl
+     * @param array $extent
+     * @return array
+     * @author Robert
+     */
+    public function dial(string $mobile, string $caller, string $callee, string $notifyUrl, array $extent = []): array
+    {
+        $body = $this->http('post', '/api/CallRequest', $this->protocol([
+            'Callid' => $this->makeCallId(),
+            'App_id' => $this->appId,
+            'Caller' => $caller,
+            'Callee' => $callee,
+            'Call_minutes' => '',//最大呼叫时间(单位分钟),不大于500分钟
+            'Extends' => json_encode($extent),//扩展字段
+            'Cdr_url' => $notifyUrl,//接收推送话单的url
+        ]), $this->getToken());
+        if (!$body || !$body = json_decode($body, true)) {
+            throw new RuntimeException('请求失败');
+        }
+        if (isset($body['result']) || $body['result'] != 0) {
+            throw new RuntimeException($body['msg'] . ' code:' . $body['result']);
+        }
+        return $body;
+    }
+
+    /**
+     * @return string
+     * @author Robert
+     */
+    private function makeCallId(): string
+    {
+        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0x0fff) | 0x4000,
+            mt_rand(0, 0x3fff) | 0x8000,
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        );
+    }
+
+    /**
+     * @param array $data
+     * @return string
+     * @author Robert
+     */
+    private function protocol(array $data): string
+    {
+        return json_encode($data);
+    }
+
+    /**
+     * @param string $method
+     * @param string $route
+     * @param string $body
+     * @param string $token
+     * @return string
+     * @author Robert
+     */
+    private function http(string $method, string $route, string $body = '', string $token = ''): string
+    {
+        $saber = Saber::create([
+            'base_uri' => $this->host,
+            'headers' => [
+                'Content-Type' => 'Content-Type:application/json',
+            ],
+        ]);
+        if ($token) {
+            $saber['headers'][] = 'Authorization:JH ' . $token;
+        }
+        $response = $saber->$method($route, $body);
+        if (!$response->isSuccess()) {
+            throw new RuntimeException('oss failed');
+        }
+        $body = (string)$response->getBody();
+        if (!$body) {
+            throw new RuntimeException('oss failed');
+        }
+        return $body;
+    }
+
+    /**
+     * 通知接受
+     * @param string $raw
+     * @return array
+     * @author Robert
+     */
+    public function notify(string $raw): array
+    {
+        $data = json_decode($raw, true);
+        //{"App_id":"200","Call_answertime":"2017-08-22 15:54:10","Call_bill":0.07,"Call_duration":6,"Call_endtime":"2017-08-22 15:54:16","Call_id":"9a530b67-8742-11e7-a924-00163e0ea174appIp:116.231.90.150","Call_starttime":"2017-08-22 15:54:01","Callee":"+8617717028007","Caller":"+8618939892185","Request_time":"2017-08-22 23:53:46"}
+        if (!$data) {
+            throw new \RuntimeException('电话不存在');
+        }
+        return $data;
+
+    }
+
+
+}
